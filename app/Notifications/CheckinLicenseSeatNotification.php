c8a53f67048b8b0d760a4afd43d7fec5fdab9f87
aquadiode <csit.digetech@gmail.com>
2022-08-17 13:55:06 +0000

    removing generic webhook from notificiations

--- app/Notifications/CheckinLicenseSeatNotification.php
+++ app/Notifications/CheckinLicenseSeatNotification.php
@@ -56,11 +56,6 @@ class CheckinLicenseSeatNotification extends Notification
        return NotificationIntegrations::msteamsMessageBuilder($this->item, $this->target, $this->admin, $this->direction, $this->note, $this->expected_checkin);
     }
 
-    public function toWebhook($notifiable)
-    {
-        return NotificationIntegrations::webhookMessageBuilder($this->item, $this->target, $this->admin, $this->direction, $this->note, $this->expected_checkin, $this->settings->slack_botname);
-    }
-
     public function toDiscord($notifiable)
     {
         return NotificationIntegrations::slackMessageBuilder($this->item, $this->target, $this->admin, $this->direction, $this->note, $this->expected_checkin, $this->settings->discord_botname);
